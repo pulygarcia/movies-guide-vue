8732a8bebf35f42dbcb363cf96451d662570a249
puly garcia <pulygarcia09@gmail.com>
2024-01-30 15:18:21 -0300

    little change in separators

--- src/components/HeadingSeparator.vue
+++ src/components/HeadingSeparator.vue
@@ -7,8 +7,8 @@
 </script>
 
 <template>
-    <div class="flex items-center gap-2 py-10">
-        <h2 class="text-white font-bold text-4xl">{{ title }}</h2>
-        <span class="h-[2px] w-full bg-red-500"></span>
+    <div class="py-10">
+        <h2 class="text-white font-bold text-3xl">{{ title }}</h2>
+        <span class="h-[1px] w-full bg-red-500 block mt-2"></span>
     </div>
 </template>
